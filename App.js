f0d38895cff65d578a1037dabc91c115fd31a78d
Luis Miguel Del Gallego <alucardluis@gmail.com>
2017-11-22 18:57:38 -0500

    React native init

--- App.js
+++ App.js
@@ -12,17 +12,21 @@ import {
 import {StackNavigator} from 'react-navigation';
 import Swipeout from 'react-native-swipeout';
 import Icon from 'react-native-vector-icons/Ionicons';
- 
 
+import MapView from 'react-native-maps';
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 export class listViewMovie extends Component {
 
 
   static navigationOptions = ({ navigation, screenProps}) => ({
     title: 'Home',
-    headerRight: <Button 
-        title="Add" 
-        color="red" 
+    headerRight: <Button
+        title="Add"
+        color="red"
         onPress={() => navigation.navigate("AddMovie")} />
   });
 
@@ -35,7 +39,7 @@ export class listViewMovie extends Component {
       dataSource: ds.cloneWithRows([])
     }
 
-    
+
   }
 
   componentDidMount(){
@@ -67,7 +71,7 @@ export class listViewMovie extends Component {
           dataSource={this.state.dataSource}
           renderRow={this.renderRow.bind(this)}
         />
-        
+
       </View>
     );
   }
@@ -77,7 +81,7 @@ export class listViewMovie extends Component {
   }
 
   renderRow(dataRow){
-    
+
     // Buttons
     let swBtnRight = [
       {
@@ -95,7 +99,7 @@ export class listViewMovie extends Component {
       }
     ]
     return (
-      <Swipeout right={swBtnRight} left={swBtnLeft} autoClose={true} backgroundColor="white" buttonWidth={54}> 
+      <Swipeout right={swBtnRight} left={swBtnLeft} autoClose={true} backgroundColor="white" buttonWidth={54}>
         <TouchableHighlight onPress={ ()=> this.pressCell(dataRow)}>
             <View style={styles.cell}>
               <Text>{dataRow}</Text>
@@ -111,12 +115,93 @@ export class newView extends Component{
     title: `Details ${navigation.state.params.details.data}`,
     headerRight: <Button title="Edit" onPress={() => navigation.navigate("EditDetails")}/>
   });
+
+  constructor() {
+    super();
+    this.state = {
+      stateNav: {},
+      markers:[],
+      initialRegion: {
+        latitude: 10.9838942,
+        longitude: -74.8531227,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  }
+
+  componentWillMount(){
+    let listRestaurant = []
+    fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBVcy4mmskrCYjV6lWuoX8sG4YTZo9lFQI&location=11.0101667,-74.8117972&radius=500&type=restaurant')
+    .then((res) => { return res.json() })
+    .then((resJson) => {
+      let restaurants = resJson.results;
+      console.log(restaurants);
+      for (let index = 0; index < restaurants.length; index++) {
+
+        let obj = {
+          latlng: { latitude: restaurants[index].geometry.location.lat, longitude: restaurants[index].geometry.location.lng },
+          title: restaurants[index].name,
+          description: restaurants[index].vicinity,
+          color: 'yellow'
+        }
+
+        listRestaurant.push(obj);
+      }
+      this.setState({ markers: listRestaurant})
+
+    })
+
+    this.watchId = navigator.geolocation.getCurrentPosition(
+      (position) => {
+            this.setState({
+              initialRegion:{
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+              }
+            });
+            let myPos = {
+              latlng: { latitude: position.coords.latitude, longitude: position.coords.longitude },
+              title: "Me",
+              color: "#029e74"
+            }
+            listRestaurant.push(myPos);
+          },
+          (error) => this.setState({ error: error.message }),
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+
+    this.setState({
+      stateNav: this.props.navigation.state
+    });
+
+  }
+
+  componentWillUnmount() {
+      navigator.geolocation.clearWatch(this.watchId);
+  }
+
   render(){
-    const {state} = this.props.navigation;
     return(
       <View style={styles.containerDetails}>
-        <Text>{state.params.details.data}</Text>
-        <Text>{state.params.details.name}</Text>
+        <Text>{this.state.stateNav.params.details.data}</Text>
+        <Text>{this.state.stateNav.params.details.name}</Text>
+        <MapView
+          style={styles.map}
+          initialRegion={this.state.initialRegion}
+        >
+          {this.state.markers.map((marker,key) => (
+            <MapView.Marker
+              key={key}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+              pinColor={marker.color}
+            />
+          ))}
+        </MapView>
       </View>
     )
   }
@@ -169,6 +254,10 @@ const styles = StyleSheet.create({
   btnIcons:{
     textAlign: 'center',
     paddingTop: 15
+  },
+  map:{
+    width: "100%",
+    height: 450
   }
 });
 
